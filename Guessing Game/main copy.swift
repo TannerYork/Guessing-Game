6d050d5ee9a08036f55a518cab3b94dda04b9431
Tanner York <tanneryork@MacBook-Pro-4.local>
2018-08-24 10:30:23 -0500

    FixCrash

--- Guessing Game/main copy.swift
+++ Guessing Game/main copy.swift
@@ -12,16 +12,12 @@ func PartOne() {
 var playingGame = true
 
 repeat {
-    // Create a random number and welcome/promt user to enter a number
-    var randomNumber = Int(arc4random_uniform(101))
-//    print("Correct Answer \(randomNumber)")
-    print("Welcome to the Guessing Game")
-    print("Choose a number between 1 and 100")
-    
+    let randomNumber = Int(arc4random_uniform(101))
+    print("Welcome to the Guessing Game!")
+    print("Choose a number between 1 - 100")
     var userGuess = Int(readLine()!)
     var numOfUserGuess = 4
     
-    //There is a problem here on line 40
     while userGuess != randomNumber && numOfUserGuess > 0{
         
         if userGuess == nil {
@@ -31,18 +27,27 @@ repeat {
         if userGuess! > 100 || userGuess! <= 0 {
             print("Invaled Number")
             userGuess = Int(readLine()!)
+            if userGuess == nil {
+                print("Invaled number, try agian>")
+                userGuess = Int(readLine()!)
         }
         if userGuess! > randomNumber {
             print(" TOO HIGH! Try agian.")
             print("You have \(numOfUserGuess) guesses left")
             numOfUserGuess -= 1
             userGuess = Int(readLine()!)
+            if userGuess == nil {
+                print("Invaled number, try agian>")
+                userGuess = Int(readLine()!)
         }
         if userGuess! < randomNumber {
             print("TOO LOW! Try agian.")
             print("You have \(numOfUserGuess) guesses left")
             numOfUserGuess -= 1
             userGuess = Int(readLine()!)
+            if userGuess == nil {
+                print("Invaled number, try agian>")
+                userGuess = Int(readLine()!)
         }
         if numOfUserGuess == 0 {
             print("GAME OVER")
@@ -52,28 +57,9 @@ repeat {
         if userGuess == randomNumber {
             print("YOU WIN")
             print("Want to play again, Y/N?")
-            //Asking user if they want to play agian or not.
-            
-            
-            //This doesn't work beacuse if the user doens't enter y or n and its still a string it will start agian anyways.
-            //        var playAgain = String(readLine()!)
-            //
-            //        if playAgain == nil {
-            //            print("Error")
-            //            playAgain = String(readLine()!)
-            //        } else if playAgain == "Y" {
-            //            playingGame = false
-            //            playingGame = true
-            //        } else if playAgain == "N" {
-            //            playingGame = false
-            //            print("Good Bye!")
-            //
-            //        }
-            //Try this instead
-            //Get user input to check
+           
             var stillPLaying = readLine()!.uppercased()
             
-            //If anything other than Y or N is entered it will coniunuously ask them to enter Y or N
             while stillPLaying != "Y" && stillPLaying != "N" {
                 print("Would you like to play again Y/N?")
                 stillPLaying = readLine()!.uppercased()
